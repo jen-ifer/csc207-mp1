f04479188b6addfc6b42898af6feb2dc18473890
jenifer <silvajen@grinnell.edu>
2024-09-14 22:22:05 -0500

    I was doing the Cipher file

--- src/main/java/edu/grinnell/csc207/main/AllCaesar.java
+++ src/main/java/edu/grinnell/csc207/main/AllCaesar.java
@@ -6,25 +6,26 @@ import edu.grinnell.csc207.util.CipherUtils;
 public class AllCaesar {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    if(args.length == 3){
+    if(args.length != 3){
+      System.err.println("I only accept 3 arguments!");
+      pen.close();
+      return;
+    }
       String encoOrDeco = args[1];
       String str = args[2];
-      if(encoOrDeco == "encode"){
+      if(encoOrDeco.equals("encode")){
         for (char ch = 'a'; ch <= 'z'; ch++) {
           pen.printf("n = %c: %s\n", ch, CipherUtils.caesarEncrypt(str, ch));
         }
-        pen.close();
-
       }
-      else{
-        if (encoOrDeco == "decode"){
+      else if(encoOrDeco.equals("decode")){
           for (char ch = 'a'; ch <= 'z'; ch++) {
             pen.printf("n = %c: %s\n", ch, CipherUtils.caesarDecrypt(str, ch));
-          }
-          pen.close();
-          
+          } 
         }
+        else{
+          System.err.println("you didn't use encode or decode!");
+        }
+        pen.close();
       }
   }
- }
-}
--- src/main/java/edu/grinnell/csc207/main/Cipher.java
+++ src/main/java/edu/grinnell/csc207/main/Cipher.java
@@ -5,7 +5,17 @@ import java.io.PrintWriter;
 public class Cipher {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
+    String encoDeco;
+    String caesarOrVig;
     for (int i = 0; i < args.length; i++) {
+      if(args[i].equals("-encode")){
+  
+      }
+
+
+
+
+
       pen.printf("args[%d] = \"%s\"\n", i, args[i]);
     }
     pen.close();
